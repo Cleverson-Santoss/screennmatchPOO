f293b57d23a10841907dd093cbb77ee8f50c1a68
cleverson <cleverson.cmc@gmail.com>
2025-03-06 00:25:15 -0300

    Encapsulamento

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import br.com.alura.screenmatch.modelos.Filme;
 
 public class Principal {
+    public static void main(String[] args) {
+        //Instanciando um objeto da Classe br.com.alura.screenmatch.modelos.Filme
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.pegaMedia());
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+    }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,29 +1,64 @@
+package br.com.alura.screenmatch.modelos;
+
 public class Filme {
-    /* Abstração: Ex: (Todo Filme tem) Abaixo são os atributos do filme */
+    /* Abstração: Ex: (Todo br.com.alura.screenmatch.modelos.Filme tem) Abaixo são os atributos do filme */
     //Atributos:
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
+
+    //Metodo getter permite que seja lido o atributo privado
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
 
     //Metodo Acessor: Como o atributo está como Private, é criado um método para retornar o valor, mas sem poder altera-lo
-
-    int getTotalDeAvaliacoes(){
+    // Metodo Setter para permitir que seja alterado o atributo privado
+    public int getTotalDeAvaliacoes(){
         return totalDeAvaliacoes;
+    }
+    // O setter permite modificar o valor de forma controlada.
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
     //Método sem retorno
 
-    void exibeFichaTecnica(){
-        System.out.println("Nome do Filme: " + nome);
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do br.com.alura.screenmatch.modelos.Filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
     //metodo sem retorno com argumentos
 
-    void avalia(double nota){
+    public void avalia(double nota){
         //está somando as avaliações cada vez que receber um valor
         somaDasAvaliacoes += nota;
         //Esta contanto o número de avaliações
@@ -32,7 +67,7 @@ public class Filme {
 
     //Método com um retorno
 
-    double  pegaMedia(){
+    public double  pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
